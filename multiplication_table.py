57ad42460a8122e54b5833de93e31f872bf7f79b
engrvitalis <ndlinks@gmail.com>
2020-12-27 00:32:33 +0100

    More corrections.

--- multiplication_table.py
+++ multiplication_table.py
@@ -6,18 +6,22 @@ with rows and column headers.
 Input: none
 Output: String
 """
+
+start = 1
+stop = 10
+
 # Create the column headers.
 print("    ", end="")
-for k in range(1, 11) :
+for k in range(start, stop+1) :
     print(f"{k:3}", end=" ")
 
 print("   ", end="")
 print()
 
-for i in range(1, 11):
+for i in range(start, stop+1):
     # Create row headers.
     print(' ', end=" ")
     print(i, end=" ")
 
     # Populate the body with rows x columns.
-    print(*(f"{i*j:3}" for j in range(1, 11)))
+    print(*(f"{i*j:3}" for j in range(start, stop+1)))
