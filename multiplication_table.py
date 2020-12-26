d188741e56dd43965f7f9a4581e2f855d5588f9a
engrvitalis <ndlinks@gmail.com>
2020-12-27 00:22:35 +0100

    Multiplication table with rows and column headers.

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,10 +1,23 @@
 """
 Project: Multiplication table
 Description: This program displays multiplication table from 1 to 10
+with rows and column headers.
 
 Input: none
 Output: String
 """
+# Create the column headers.
+print("    ", end="")
+for k in range(1, 11) :
+    print(f"{k:3}", end=" ")
+
+print("   ", end="")
+print()
 
 for i in range(1, 11):
+    # Create row headers.
+    print(' ', end=" ")
+    print(i, end=" ")
+
+    # Populate the body with rows X columns.
     print(*(f"{i*j:3}" for j in range(1, 11)))
