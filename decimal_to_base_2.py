02f513050c9568c4de36f042daa3211d06bf75df
engrvitalis <ndlinks@gmail.com>
2020-12-27 12:06:51 +0100

    Adding relevant comments for clarity.

--- decimal_to_base_2.py
+++ decimal_to_base_2.py
@@ -4,6 +4,7 @@ This project converts a number to in base 10 to base 2.
 Input: Integer
 Output: String
 '''
+# Request for user input and initialize state variables.
 print()
 print("This program converts base 10 integers to base 2")
 print("Please enter an integer below:")
@@ -11,11 +12,13 @@ q = int(input("--> "))
 q_copy = q
 result = ''
 
+# Do long division and accumulate the remainders.
 while q > 0:
     r = q % 2
     q = q // 2
     result = str(r) + result
 
+# Diplay the result
 print(f'Base 2 of {q_copy} is {result}')
 
 
