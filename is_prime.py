44ce5fe62401ea4c16a56a58c62714bd47d77ce1
engrvitalis <ndlinks@gmail.com>
2020-12-28 23:52:58 +0100

    Initial commint

--- is_prime.py
+++ is_prime.py
@@ -5,3 +5,18 @@ Input: Integer
 Output: String
 """
 
+def is_prime(num):
+    if num == 1:
+        return f'{num} is not prime.'
+    
+    for i in range(2, num):
+        if num % i == 0:
+            return f'{num} is not prime'
+        
+    return f'{num} is a prime'
+
+
+if __name__ == "__main__":
+    num = int(input("Enter an integer: "))
+
+print(is_prime(num))
