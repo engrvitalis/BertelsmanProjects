62b9c79cba64020de0d72d6d5b46aaf49a260c2e
engrvitalis <ndlinks@gmail.com>
2020-12-29 23:56:16 +0100

    Initial commit

---
+++ check_password.py
@@ -0,0 +1,29 @@
+"""
+This program check if a password is good by using the following criteria:
+- It must contain at least 8 characters.
+- It must contain at least one uppercase letter.
+- It must contain at least one lowercase letter.
+- It must contain at least one number.
+
+Input: String
+Output: String
+"""
+
+def check_password(password):
+    # Initialize state variables.
+    uppercase = False
+    lowercase = False
+    digit = False
+
+    # Check for password compliance with specification.
+    for char in password:
+        if char.isupper():
+            uppercase = True
+        if char.islower():
+            lowercase = True
+        if char.isdigit:
+            digit = True
+    
+    return (len(password) >= 8 and uppercase and lowercase and digit)
+
+    
