90684321ec35b3cbc417149b957d7bf2f7d7d01a
engrvitalis <ndlinks@gmail.com>
2020-12-31 23:18:18 +0100

    Type cast Debugging.

--- base_converter.py
+++ base_converter.py
@@ -16,20 +16,19 @@ def decimal_to_others(num, new_base):
 
     # Request for user input and initialize state variable.
     result = ''
+    num = int(num)
+    new_base = int(new_base)
 
     # Do long division and accumulate the remainders.
-    if 2 <= new_base <= 16:
-        while num > 0:
-            rem = num % new_base
-            if rem >= 10:
-                rem = chr(55 + rem) # Map to appropriate letter if remainder is less greater than 9.
-            num = num // new_base
-            result = str(rem) + result
+    while num > 0:
+        rem = num % new_base
+        if rem >= 10:
+            rem = chr(55 + rem) # Map to appropriate letter if remainder is less greater than 9.
+        num = num // new_base
+        result = str(rem) + result
 
-        return result
-    else:
-        print("Base must be between 2 and 16!")
-        return -1
+    return result
+
 
 def others_to_decimal(num, num_base):
     result = 0
@@ -37,9 +36,9 @@ def others_to_decimal(num, num_base):
 
     for i in num:
         if ord('A') <= ord(i) <= ord('Z'):
-            result += (ord(i) - 55) * num_base**(power)
+            result += (ord(i) - 55) * int(num_base)**(power)
         else:
-            result += int(i) * num_base**(power)
+            result += int(i) * int(num_base)**(power)
         power -= 1
 
     return result
@@ -48,17 +47,17 @@ def main():
     print('Please enter the type of conversion to perform: ')
     print('- Enter 1 for "Decimal to other bases conversion"\n- Enter 2 for "Other bases to Decimal conversion"')
     conv_type = int(input('--> '))
-    if conv_type == 1:
-        num = int(input("Enter number: "))
-        base = int(input("Enter number base: "))
 
-        print(f'{num} to base 10 = {decimal_to_others(num, base)} to base {base}.')
+    num = input("Enter number: ")
+    base = input("Enter base: ")
+    if 2 <= int(base) <= 16:
+        if conv_type == 1:
+            print(f'{num} to base 10 = {decimal_to_others(num, base)} to base {base}.')
 
-    if conv_type == 2:
-        num = input("Enter number: ")
-        base = int(input("Enter number base: "))
-
-        print(f'{num} to base {base} = {others_to_decimal(num, base)} to base 10')
+        if conv_type == 2:
+            print(f'{num} to base {base} = {others_to_decimal(num, base)} to base 10')
+    else:
+        print('Invalid input!\nBase must be between 2 and 16!')
 
 if __name__ == '__main__':
     main()
