0ea463d730bb5cbf6a57942b4a3a390b3b7cfc39
engrvitalis <ndlinks@gmail.com>
2021-01-01 23:12:01 +0100

    Debugging and comments added.

--- reduce_measure.py
+++ reduce_measure.py
@@ -4,46 +4,58 @@ This program expresses imperial volume using the largest possible units.
 
 def reduce_measure(num, unit):
     if unit == "teaspoon":
-        cup = num//(16*3)
+        cup = num // (16*3)
         tablespoon = (num % (16*3)) // 3
         teaspoon = (num % (16*3)) % 3
         return (cup, tablespoon, teaspoon)
 
-    elif unit == "tablespoon":
+    else:
         cup = num // 16
         tablespoon = num % 16 
         return (cup, tablespoon)
-    else:
-        return num
 
 def tense(tup):
     """
     This function takes a tuple of numbers and return 
-    a tuple of strings.
+    a tuple of strings. It makes sure that unit of measures are 
+    displayed as either singular or plural words.
     """
-    v = ('cup', 'tablespoon', 'teaspoon')
+    # Initialize variables.
+    u = ('cup', 'tablespoon', 'teaspoon')
     ls = []
     index = 0
+
+    # Go through the units of measure and append 's' if value is more than one.
     for i in tup:
         if i > 1:
-            ls.append(str(v[index]) + 's')
+            ls.append(str(u[index]) + 's')
         else:
-            ls.append(v[index])
+            ls.append(u[index])
         index += 1
     
     return tuple(ls)
 
 def main():
-    x = 59
-    y = "teaspoon"
-    m = reduce_measure(x, y)
-    t = tense(m)
+    # Request users input.
+    x = int(input("Enter number: "))
+    y = input("Enter unit of measure (tablespoon or teaspoon): ")
 
-    result = ""
-    for i in range(len(m)):
-        result += f', {m[i]} {t[i]}'
+    # Check for invalid input.
+    if y == 'tablespoon' or y == 'teaspoon':
+        m = reduce_measure(x, y)
+        t = tense(m)
+        result = ""
 
-    print(result[2:])
+        # Format output.
+        for i in range(len(m)):
+            if m[i] == 0:
+                continue
+            result += f', {m[i]} {t[i]}'
+
+        print(result[2:])
+
+    else:
+        print("invalid Entry!")
  
 if __name__ == '__main__':
     main()
