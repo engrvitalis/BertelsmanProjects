b88272a2ebd4ec31deda0ba868e83672aec2d2f5
engrvitalis <ndlinks@gmail.com>
2021-01-02 21:54:38 +0100

    Commenting.

--- sorted_order.py
+++ sorted_order.py
@@ -4,25 +4,34 @@ the values one line at a time.
 """
 
 def sort_num():
-
-    # Request input from user.
-    num = int(input("Enter a number: "))
-
-    ls = list()
-
-    while num != 0:
-        ls.append(num)
+    try:
+        # Request initial input from user.
         num = int(input("Enter a number: "))
 
-    return sorted(ls)
+        # Initialize state variable.
+        ls = list()
+
+        # Continuously ask for input and collect 0 is entered.
+        while num != 0:
+            ls.append(num)
+            num = int(input("Enter a number: "))
+
+        return sorted(ls)
+    except:
+        print("Invalid Input!")
+        return []
 
 def display(ls):
+    """
+    Loop through the list argument and print the content on separate lines.
+    """
+
     for i in ls:
         print(i)
 
 def main():
 
-    # Collect and sort the numbers.
+    # Collect numbers from user and sort them in ascending order.
     ls = sort_num()
 
     # Display the result
