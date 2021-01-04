facb718a99ffa1657bd506f902183a9a7fff6a3b
engrvitalis <ndlinks@gmail.com>
2021-01-04 20:41:43 +0100

    Initial commit

---
+++ remove_duplicates.py
@@ -0,0 +1,24 @@
+"""
+This program reads words from user until the user enters
+a blank line.
+
+@param: words(String). To enter the next one, hit return after the first.
+        To stop data entry, hit return twice after the last word.
+@return: The list of words entered in the same sequence they were entered 
+         without duplicates.
+"""
+
+def remove_duplicates(words):
+    list_of_words = list()
+
+    while True:
+        word = input("Enter a word or hit return to terminate: ")
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
