fec85a647aa2bbc6eb4f84a864576deb316fb281
engrvitalis <ndlinks@gmail.com>
2021-01-04 21:19:39 +0100

    Debugging. Syntax error.

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -2,22 +2,25 @@
 This program reads words from user until the user enters
 a blank line.
 
-@param: words(String). To enter the next one, hit return after the first.
+@param: words(String). To enter the next word, hit return after the first.
         To stop data entry, hit return twice after the last word.
 @return: The list of words entered in the same sequence they were entered 
          without duplicates.
 """
 
-def remove_duplicates(words):
+def remove_duplicates():
     list_of_words = list()
 
     while True:
         word = input("Enter a word or hit return to terminate: ")
+        if word == '':break
+        elif word not in list_of_words:
+            list_of_words.append(word)
 
-
+    return list_of_words
 
 def main():
-    pass
+    print(f'{remove_duplicates()}')
 
 
 if __name__ == '__main__':
