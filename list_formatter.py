4150ffa2a193f3ecf258e5916fa63230e3c98a07
engrvitalis <ndlinks@gmail.com>
2021-01-06 22:31:50 +0100

    Docstring completed.

--- list_formatter.py
+++ list_formatter.py
@@ -1,2 +1,9 @@
 """
-This program reads 
+This program reads successive input of words from the user,
+collect them in a list and format them appropriate separators between them.
+
+@params: String - words. Separate them by hitting return after each word.
+         Hit return twice to end entry.
+@return: String of words formatted as per specification.
+"""
+
