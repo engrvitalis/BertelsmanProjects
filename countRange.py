04d1cff58e887a8fe56947a2805b4c57d166b0e5
engrvitalis <ndlinks@gmail.com>
2021-01-09 16:30:38 +0100

    Initial commit.

---
+++ countRange.py
@@ -0,0 +1,14 @@
+"""
+This program will return the total number of elements within a 
+particular range in a list.
+
+@params: List - The parameter to be processed.
+         min_value - Minimum value of the range.
+         max_value - Maximum value of the range.
+@return: Integer - Elements count from and including min_value
+         and up to but excluding max_value.
+         ie. min_value <= element < max_value.
+"""
+
+def countRange(lst, min_value, max_value):
+    
