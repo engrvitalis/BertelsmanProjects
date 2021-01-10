533a8e692d6c1d7a89f29c1044f5cb1e94348671
engrvitalis <ndlinks@gmail.com>
2021-01-10 23:48:20 +0100

    Structure changed.

--- countRange.py
+++ countRange.py
@@ -21,97 +21,46 @@ def countRange(ls, min_value, max_value):
         if min_value <= num < max_value:
             count += 1
 
-    # print(f'List: {ls} | Min: {min_value} | Max: {max_value}')
-
     return count
 
+def create_list(num_cases=10, min_size=3, max_size=10, start_range=-100, end_range=100):
+    # Generating several test cases.
+    import random
 
-def validate_number(num):
-    """
-    Validate number input from user.
+    # Initialize state variables.
+    main_ls = list()
 
-    @param: String eg. '3', '2.3' etc.
-    @return Boolean eg. (True, 'integer', int(num)), (True, 'float', float(num)) or False.
-    """
+    # Generate random list with minimum and maximum values
+    # and run test.
+    for i in range(num_cases):
+        # Initialize variables.
+        ls = list()
+        list_size = random.randrange(min_size, max_size)
 
-    try:
-        return (True, 'integer', int(num))
-    except ValueError:
-        try:
-            return (True, 'float', float(num))
-        except ValueError:
-            return False
+        # Create list for test.
+        for j in range(list_size):
+            num = random.randrange(start_range, end_range)
+            ls.append(num)
+        main_ls.append(ls)
 
+    return tuple(main_ls)
 
 
 def main():
-    # Initialize state variables.
-    ls = list()
+    # Generate some lists.
+    import random
 
-    # Request and Validate Input of number(s) from user.
-    while True:
-        num = input('Enter a number or blank line to stop): ')
-        if num == '':
-            break
-        else:
-            if validate_number(num)[1] == 'integer':
-                ls.append(int(num))
-            elif validate_number(num)[1] == 'float':
-                ls.append(float(num))
-            else:
-                print("Invalid number. Please enter a float or int!")
-                continue
-    
-    print()
-    # Continue only if user provided at least one valid numbers to process.
-    if len(ls) != 0:
-        # Request and Validate input values.
-        while True:
-            min_value = input("Provide a minimum value to define a range: ")
-            max_value = input("Provide a maximum value to define a range: ")
-            # Check for min_value == max_value senario.
-            if min_value != '' and max_value != '' and min_value == max_value:
-                print(f'Minimum value cannot be equal to Maximum value: Min = {min_value}; Max = {max_value}')
-                continue
-            # Validate minimum value.
-            if min_value != "":
-                if validate_number(min_value)[1] == 'integer':
-                    min_value = int(min_value)
-                elif validate_number(min_value)[1] == 'float':
-                    min_value = float(min_value)
-                else:
-                    print("Your minimum value is not a number! Please, re-enter values!")
-                    continue
-            # Validate maximum value.
-            if max_value != "":
-                if validate_number(max_value)[1] == 'integer':
-                    max_value = int(max_value)
-                    print(f'max1: {max_value}')
-                elif validate_number(max_value)[1] == 'float':
-                    max_value = float(max_value)
-                    print(f'float: {max_value}')
-                else:
-                    print("Your maximum value is not a number! Please, re-enter values!")
-                    continue
 
-            break
+    t = create_list()
+    for l in t:
+        while min_val > max_val:
+            min_val = random.randrange(min(l), max(l))
+            max_val = random.randrange(min(l), max(l))
         
-        # Continue if there is valid data to process.
-        if len(ls) != 0:
-            # Set default values incase of blank line entry by user
-            # for minimum or maximum values or both.
-            if min_value == "" and max_value == "":
-                min_value = min(ls)
-                # max_value = max(ls) + 1
-            elif min_value == "":
-                min_value = min(ls)
-            else:
-                max_value = max(ls)
-            print(f'Min: {min_value}, Max: {max_value}, List: {ls}')
-            # Count values based on the minimum and maximum values.
-            print(countRange(ls, min_value, max_value))
+
+        
+
+
 
 if __name__ == "__main__":
-    # main()
-    # print(countRange([2.2, 3.5, 2, 5, 7.1, 8], 2, 7.1))
-    # print(validate_number('l'))
+    main()
