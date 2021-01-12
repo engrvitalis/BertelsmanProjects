3d3110db6ba60408bd61f78509d08cc20dd820e1
engrvitalis <ndlinks@gmail.com>
2021-01-12 22:35:43 +0100

    Commenting of create_dict function completed.

--- reverse_lookup.py
+++ reverse_lookup.py
@@ -11,6 +11,7 @@ def reverseLookup(dic, value):
     # Initialize variable
     ls = list()
 
+    # Search for value in dictionary and the key in a list if found.
     for k, v in dic.items():
         if v == value:
             ls.append(k)
@@ -19,23 +20,42 @@ def reverseLookup(dic, value):
 
 
 def create_dict():
+    """
+    This program returns a list containing automatically generated
+    dictionaries, ie. a list of dictionaries with the length of the 
+    list being equal to num_cases.
+
+    @param: None
+    @return: List of dictionaries.
+    """
+
     import random
 
 
+    # Initialize state variables.
     ls = list()
     num_cases = 10
 
     for i in range(num_cases):
+
+        # Initialize variables.
         dic_len = random.randrange(2, 10)
         dic = dict()
+
         for j in range(dic_len):
+            # Generate length of proposed key.
             key_len = random.randrange(3, 5)
+            # Generate list of characters with length key_len.
             key_list = [chr(random.randrange(ord('a'), ord('z'))) for i in range(key_len)]
+            # Convert key_list to string to get a random key.
             key = ''.join(key_list)
+            # Generate a random value.
             value = random.randrange(0, 20)
+            # Populate dic with key value pair.
             if key not in dic:
                 dic[key] = value
-
+        
+        # Populate ls with dic.
         ls.append(dic)
 
     return ls
