df3f4582a5b884542d263450aea0634e85591976
engrvitalis <ndlinks@gmail.com>
2021-01-12 21:26:15 +0100

    reverseLookup function completed.

--- reverse_lookup.py
+++ reverse_lookup.py
@@ -1,4 +1,4 @@
-def reverseLookup():
+def reverseLookup(dic, value):
     """
     This program takes two arguments, a dictionary and a value
     and tries to lookup the corresponding keys for the value.
@@ -8,4 +8,19 @@ def reverseLookup():
     @return: list of keys.
     """
 
-    
+    # Initialize variable
+    ls = list()
+
+    for k, v in dic.items():
+        if v == value:
+            ls.append(k)
+
+    return ls
+
+
+def main():
+    pass
+    # print(reverseLookup({'me': 4, 'you': 6, 'i': 7, 'jjs': 7}, 8))
+
+if __name__ == "__main__":
+    main()
