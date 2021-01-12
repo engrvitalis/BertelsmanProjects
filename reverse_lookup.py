f5524a9e7848088c8c191eb0358813a030299d19
engrvitalis <ndlinks@gmail.com>
2021-01-12 21:12:02 +0100

    Documenting reverseLookup function.

--- reverse_lookup.py
+++ reverse_lookup.py
@@ -1,4 +1,11 @@
 def reverseLookup():
-    pass
+    """
+    This program takes two arguments, a dictionary and a value
+    and tries to lookup the corresponding keys for the value.
+
+    @param: Dictionary
+            value - int, float, str etc.
+    @return: list of keys.
+    """
 
     
