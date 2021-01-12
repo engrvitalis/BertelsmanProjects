2816efcbf46ee3e7b82c2cfa2094900d6c946df3
engrvitalis <ndlinks@gmail.com>
2021-01-12 13:33:02 +0100

    Initial commit.

---
+++ sieve.py
@@ -0,0 +1,8 @@
+def sieve(num):
+    """
+    This function uses sieve of eratosthenes algorithm to compute 
+    a list of prime numbers within a range.
+
+    @param: Integer - num
+    @return: Integers
+    """
