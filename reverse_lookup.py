97c5446c3602849fe26290ca166c923e198fdba7
engrvitalis <ndlinks@gmail.com>
2021-01-12 22:15:40 +0100

    create_dict function completed.

--- reverse_lookup.py
+++ reverse_lookup.py
@@ -19,10 +19,33 @@ def reverseLookup(dic, value):
 
 
 def create_dict():
+    import random
+
+
+    ls = list()
+    num_cases = 10
+
+    for i in range(num_cases):
+        dic_len = random.randrange(2, 10)
+        dic = dict()
+        for j in range(dic_len):
+            key_len = random.randrange(3, 5)
+            key_list = [chr(random.randrange(ord('a'), ord('z'))) for i in range(key_len)]
+            key = ''.join(key_list)
+            value = random.randrange(0, 20)
+            if key not in dic:
+                dic[key] = value
+
+        ls.append(dic)
+
+    return ls
+
+
+
 
 
 def main():
-    pass
+    print(create_dict())
     # print(reverseLookup({'me': 4, 'you': 6, 'i': 7, 'jjs': 7}, 8))
 
 if __name__ == "__main__":
