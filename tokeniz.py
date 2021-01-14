4b03927b42b12f5a5a10ed554b4cf6f695a95c39
engrvitalis <ndlinks@gmail.com>
2021-01-14 23:28:21 +0100

    Symantic error rectified.

--- tokeniz.py
+++ tokeniz.py
@@ -15,20 +15,30 @@ def tokeniz(string):
     string = strip_space(string)
 
     while i < len(string):
-        # 
+        # Check if character is an operator.
         if is_operator(string[i]):
+            # Check if character is either '+' or '-' and not at the
+            # beginning or end of string.
             if i != 0 and i != len(string)-1 and string[i] in ['+', '-']:
+                # Check if '+' or '-' is following a number or ')'.
                 if string[i-1] == ')' or string[i-1].isdigit():
+                    # Add '+' or '-' as an operator.
                     tokens.append(string[i])
                 else:
-                    tokens.append(''.join(string[i:i+2]))
-                    i += 1
+                    if string[i+1].isdigit():
+                        # Add as a signed number.
+                        tokens.append(''.join(string[i:i+2]))
+                        i += 1
+                    else:
+                        tokens.append(string[i])
             else:
+                # Take care of signed numbers at string extremes.
                 if string[i] in ['+', '-'] and i != len(string) - 1 and string[i + 1].isdigit():
                     tokens.append(''.join(string[i:i+2]))
                     i += 1
                 else:
                     tokens.append(string[i])
+        # Add parenthesis and numbers.
         elif string[i] in ['(', ')'] or string[i].isdigit():
             tokens.append(string[i])
         i += 1
@@ -57,13 +67,13 @@ def strip_space(s):
 
 
 def main():
-    # Initialize state variables.
-    new_s = []
     # Request input from the user.
-    # s = input("Enter mathematical expression: ")
-    s = '-2 + 3 * 6 + (3 ^ 3) * (-5) / 6+'
-    print(s)
-    print(tokeniz(s))
+    s = input("Enter mathematical expression: ")
+
+    # Print tokens.
+    print("\nIdentified tokens are:")
+    for token in tokeniz(s):
+        print(token, end=', ')
 
 
 if __name__ == "__main__":
