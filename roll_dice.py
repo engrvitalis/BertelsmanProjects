ed03e4b2d49492c16964251ada2773472b8c2ce0
engrvitalis <ndlinks@gmail.com>
2021-01-14 12:38:38 +0100

    roll_dice function complete.

--- roll_dice.py
+++ roll_dice.py
@@ -1,2 +1,17 @@
 def roll_dice():
-    
+    """
+    This function simulates a two dice roll and returns the sum as output.
+
+    @param: None.
+    @return: Int - Sum of two dice roll.
+    """
+
+    import random
+
+
+    # Roll dice.
+    dice_1 = random.randrange(1, 6)
+    dice_2 = random.randrange(1, 6)
+
+    # Return the sum.
+    return sum(dice_1 + dice_2)
