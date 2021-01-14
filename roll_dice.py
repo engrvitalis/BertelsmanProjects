9a7a521eff2e075148062e9a759a8e1068dcfa41
engrvitalis <ndlinks@gmail.com>
2021-01-14 14:34:58 +0100

    Program completed.

--- roll_dice.py
+++ roll_dice.py
@@ -20,9 +20,11 @@ def roll_dice():
 def main():
     # Initialize variable
     results = dict()
+    num_roll = 1000
+    prob_table = [0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
 
     # Simulate dice roll and document num and frequency in results.
-    for i in range(1000):
+    for i in range(num_roll):
         num = roll_dice()
         if num in results.keys():
             results[num][0] += 1
@@ -30,7 +32,17 @@ def main():
             results[num] = [0, 0, 0]
             results[num][0] += 1
 
-    print(results)
+    # Calculate the simulated percentage and insert in results.
+    for key, value in results.items():
+        results[key][1] = round((value[0] / num_roll) * 100, 2)
+        results[key][2] = round((prob_table[int(key) - 1] / 36) * 100, 2)
+    
+    # Print results
+    print(f'\nTotal\t\tSimulated\tExpected')
+    print(f'\t\tPercent\t\tPercent')
+    for key in sorted(results):
+        print(f'{key}\t\t{results[key][1]}\t\t{results[key][2]}')
+
 
 if __name__ == "__main__":
     main()
