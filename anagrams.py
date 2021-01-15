1d21153095443c18b6497747284f5a3654844bc3
engrvitalis <ndlinks@gmail.com>
2021-01-15 18:19:35 +0100

    anagram function complete

--- anagrams.py
+++ anagrams.py
@@ -1,10 +1,10 @@
-def anagrams(string):
-        """
+def anagrams(s1, s2):
+    """
     This function request two string input from the 
     user and reports whether they are anagrams.
 
-    @params: Strings (Input1, Input2)
+    @params: Strings (s1, s2)
     @return: Boolean (True or False)
     """
 
-    
+    return sorted(s1.lower()) == sorted(s2.lower())
