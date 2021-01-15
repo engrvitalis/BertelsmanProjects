5741d1e625c13698d477f7420571292416284d3d
engrvitalis <ndlinks@gmail.com>
2021-01-15 18:06:44 +0100

    Doc string completed.

--- anagrams.py
+++ anagrams.py
@@ -1,1 +1,10 @@
 def anagrams(string):
+        """
+    This function request two string input from the 
+    user and reports whether they are anagrams.
+
+    @params: Strings (Input1, Input2)
+    @return: Boolean (True or False)
+    """
+
+    
