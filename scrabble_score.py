0dced40cb9d275090d6aa63bf4953fce7fe2f583
engrvitalis <ndlinks@gmail.com>
2021-01-16 20:38:22 +0100

    Program complete>

--- scrabble_score.py
+++ scrabble_score.py
@@ -1,11 +1,14 @@
 def scrabble_score(word):
     """
     This function takes a word and return the full score
-    based on scrabble rules.
+    based on scrabble rules. It is assumed that the user
+    will enter valid English word.
 
     @param: String - word
     @return: Integer
     """
+
+    return sum([char_score(s) for s in word])
 
 
 def char_score(s):
@@ -17,20 +20,25 @@ def char_score(s):
     @return: Integer
     """
 
-    score = {"a": 1, "e": 1, "i": 1, "l": 1, "o": 1, 
+    score = {
+            "a": 1, "e": 1, "i": 1, "l": 1, "o": 1, 
             "n": 1, "s": 1, "r": 1, "u": 1, "t": 1, 
             "d": 2, "g": 2, "c": 3, "b": 3, "m": 3, 
             "p": 3, "f": 4,  "h": 4, "w": 4, "v": 4, 
             "y": 4, "k": 5, "j": 8,  "x": 8, "q": 10, 
-            "z": 10}
+            "z": 10
+            }
 
     return score[s]
              
           
-
-
 def main():
-    print(char_score('j'))
+    # Request input from user.
+    word = input("\nEnter a word: ")
+    word_lower = word.lower()
+
+    # Calculate and display word score.
+    print(f'The word "{word}" scores {scrabble_score(word_lower)} points')
 
 if __name__ == "__main__":
     main()
