a266ef2a7491689cea976e45b6b14b1aca05e8f3
engrvitalis <ndlinks@gmail.com>
2021-01-17 11:18:25 +0100

    PEP rule based adjustment of the dict, score.

--- scrabble_score.py
+++ scrabble_score.py
@@ -24,13 +24,13 @@ def char_score(s):
     # Mapping points to each English alphabet as  
     # per scrabble rules.
     score = {
-            "a": 1, "e": 1, "i": 1, "l": 1, "o": 1, 
-            "n": 1, "s": 1, "r": 1, "u": 1, "t": 1, 
-            "d": 2, "g": 2, "c": 3, "b": 3, "m": 3, 
-            "p": 3, "f": 4,  "h": 4, "w": 4, "v": 4, 
-            "y": 4, "k": 5, "j": 8,  "x": 8, "q": 10, 
-            "z": 10
-            }
+        "a": 1, "e": 1, "i": 1, "l": 1, "o": 1, 
+        "n": 1, "s": 1, "r": 1, "u": 1, "t": 1, 
+        "d": 2, "g": 2, "c": 3, "b": 3, "m": 3, 
+        "p": 3, "f": 4,  "h": 4, "w": 4, "v": 4, 
+        "y": 4, "k": 5, "j": 8,  "x": 8, "q": 10, 
+        "z": 10
+        }
 
     # Return character score.
     return score[s]
