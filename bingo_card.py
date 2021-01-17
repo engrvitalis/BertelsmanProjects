268b06ea428bf681c6c87c6c29f0dc7e2aa951f6
engrvitalis <ndlinks@gmail.com>
2021-01-17 23:13:56 +0100

    bingo_card function completed

--- bingo_card.py
+++ bingo_card.py
@@ -7,4 +7,21 @@ def bingo_card():
     @return: dictionary - Bingo card.
     """
 
-    
+
+    import random
+
+
+    char = ['B', 'I', 'N', 'G', 'O']
+    card = {}
+    start = 1
+    stop = 15
+
+    for c in char:
+        card[c] = random.sample(range(start, stop), 5)
+        start += 15
+        stop += 15
+
+    return card
+
+
+print(bingo_card())
