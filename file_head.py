5403e9f7cee6bf63f1c2ed9e4bc8d8552ba41562
engrvitalis <ndlinks@gmail.com>
2021-01-18 23:15:21 +0100

    file_head function complete.

--- file_head.py
+++ file_head.py
@@ -7,4 +7,15 @@ def file_head(filename):
     @return: None
     """
 
-    
+    # Initialize variable.
+    i = 1
+
+    # Go through the file and print first 10 lines.
+    with open(filename) as file:
+        for line in file:
+            if i <= 10:
+                print(line, end='')
+            i += 1
+
+
+file_head('elements.txt')
