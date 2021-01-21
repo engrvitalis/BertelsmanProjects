875d6ff2ab8e82d8f1b24009400671923c8c64e7
engrvitalis <ndlinks@gmail.com>
2021-01-21 23:46:26 +0100

    Commenting of code.

--- sum_num.py
+++ sum_num.py
@@ -8,27 +8,35 @@ def sum_num():
     @return: Int or Float
     """
 
+    # Initialize variable
     total = 0
 
     while True:
         print()
-        num = input("Enter a number: ")
+        num = input("Enter a number: ") # Request input from user.
         # print()
 
+        # Exit and print total so far if blank line is entered.
         if num == '':
             print(f'Total so far: {total}')
             break
         
+        # Check for invalid number.
         try:
-            total += float(num)
+            total += float(num) # Accumulate sum of numbers.
         except ValueError:
             print()
-            print("Invalid entry! Enter int or float")
 
+            # Incase of invalid entry, print relevant message.
+            print("Invalid entry. Enter int or float!")
+
+        # Continuously print total irrespective of input condition.
         print(f'Total so far: {total}')
 
 
 def main():
+    # Continuously sum up series of numbers entered by the user
+    # and display the total.
     sum_num()
 
 
