8c3560589451972285483a507b903f1f3eb171c1
engrvitalis <ndlinks@gmail.com>
2021-01-21 11:21:26 +0100

    Single file read completed

--- concat.py
+++ concat.py
@@ -1,4 +1,6 @@
-def concat(*filename):
+import sys
+
+def concat(filename):
     """
     This function concatenates and displays one or more files
     whose names are provided as command line parameters.
@@ -7,3 +9,16 @@ def concat(*filename):
     """
 
     
+    for file in sys.argv: 
+        with open(file) as f_handle:
+            for f in f_handle:
+                print(f_handle.readline(), end='')
+
+
+def main():
+    filename = sys.argv[1:]
+    concat(filename)
+
+
+if __name__ == "__main__":
+    main()
