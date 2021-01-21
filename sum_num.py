4e8b62065cdf1e8cd910cbfc0d9ea415ba97483c
engrvitalis <ndlinks@gmail.com>
2021-01-21 23:15:51 +0100

    sum_num doc-string complete

--- sum_num.py
+++ sum_num.py
@@ -1,2 +1,10 @@
 def sum_num():
-    
+    """
+    This function constantly request input of number from 
+    user and displays the total of all the numbers entered so far
+    while ignoring invalid entry.
+
+    @param: Int or Float
+    @return: Int or Float
+    """
+
