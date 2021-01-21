444c3650e604f829dd19a2a873edb316519bf41f
engrvitalis <ndlinks@gmail.com>
2021-01-21 10:50:34 +0100

    Doc-string completed.

---
+++ concat.py
@@ -0,0 +1,9 @@
+def concat(*filename):
+    """
+    This function concatenates and displays one or more files
+    whose names are provided as command line parameters.
+    @param: Strings
+    @return: None
+    """
+
+    
