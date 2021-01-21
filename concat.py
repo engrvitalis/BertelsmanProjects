21078695ad53abaf6b329cad4758968c75b74cef
engrvitalis <ndlinks@gmail.com>
2021-01-21 13:35:28 +0100

    Debugging

--- concat.py
+++ concat.py
@@ -4,20 +4,37 @@ def concat(filename):
     """
     This function concatenates and displays one or more files
     whose names are provided as command line parameters.
+    
     @param: Strings
     @return: None
     """
-
+    ls = list()
     
     for file in sys.argv: 
-        with open(file) as f_handle:
-            for f in f_handle:
-                print(f_handle.readline(), end='')
+        with open(file) as f:
+            ls.extend(f.readlines())
+        ls.append('\n')
+
+    with open('new_file.txt', 'w') as new_file:
+        for line in ls:
+            new_file.write(line)
+
+    with open('new_file.txt') as f:
+        for line in f:
+            print(line.rstrip())
 
 
 def main():
-    filename = sys.argv[1:]
-    concat(filename)
+    try: 
+        filename = sys.argv[1]
+        concat(filename) 
+
+    except FileNotFoundError:
+        print("File not found!")
+
+    except IndexError:
+        print("Please, provide a file name!")
+
 
 
 if __name__ == "__main__":
