4c14636d7f04d5d2e67ef9f69ba539a3e1f43c23
engrvitalis <ndlinks@gmail.com>
2021-01-22 21:22:34 +0100

    Program can now retrieve line that met the criteria.

--- remove_comments.py
+++ remove_comments.py
@@ -8,9 +8,26 @@ def remove_comments(old_file, new_file):
     @return: None
     """
 
+    # Declare variable.
+    
+
     with open(old_file, 'r') as old_fh:
         for line in old_fh:
-            print(line, end='')
+            # Checking for # on a line.
+            if '#' in line:
+                # print(line, end='')
+                # Checking if # is inside a string.
+                for char in ["'", '"']:
+                    if char not in line[:line.index('#')] and char not in line[line.index('#')+1:]:
+                        # Ignoring # that are inside a string.
+                        # print(line, end='')
+                        continue
+                    else:
+                        # Getting # inside a string.
+                        print(line, end='')
+            else:
+                # Getting all lines without # in them.
+                print(line, end='')
 
 
 def main():
