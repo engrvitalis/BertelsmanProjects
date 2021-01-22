38270fe317494e5f3c21b231a234f0036d0174c0
engrvitalis <ndlinks@gmail.com>
2021-01-22 20:04:49 +0100

    function can read old file.

--- remove_comments.py
+++ remove_comments.py
@@ -1,16 +1,21 @@
-def remove_comments(file):
+def remove_comments(old_file, new_file):
     """
     This function accepts a source file and creates a copy of the
     file with the comments removed.
 
-    @param: String - file name
+    @param: String - old_file: name of file to modify
+            String - new_file: name of new file to create.
     @return: None
     """
 
+    with open(old_file, 'r') as old_fh:
+        for line in old_fh:
+            print(line, end='')
+
 
 def main():
-    pass
+    print(remove_comments('elements2.txt', 'elements2_copy.txt'))
 
 
 if __name__ == "__main__":
-    pass
+    main()
