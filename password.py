f71f7ff310803a3eea3b1e6ea3bfc11688df6ef4
engrvitalis <ndlinks@gmail.com>
2021-01-24 00:41:50 +0100

    Password generator working

--- password.py
+++ password.py
@@ -8,6 +8,9 @@ def password(file):
     @return: None
     """
 
+    import random
+
+
     # Declare variable.
     ls = list()
 
@@ -18,18 +21,37 @@ def password(file):
     # Open the_industrial_republic.txt file and move words that met
     # desired specification into ls.
     with open(file, 'r') as f:
+        print('...\n...')
         for line in f:
             for word in line.split():
                 if len(word) >= 3 and word.isalpha():
                     ls.append(word)
-    print("Program loaded successfully!")
-    print(ls[:20])
-
+    print('...')
+    print("Program loaded successfully!\n")
     
+
+    option = ""
+    while True:
+        if option.lower() == 'q':
+            break
+        elif option == "":
+            while True:
+                word1 = random.choice(ls)
+                word2 = random.choice(ls)
+                if word1 == word2 or not 8 <= len(word1 + word2) <= 10:
+                    continue
+                else:
+                    print(f'Password: {word1.capitalize() + word2.capitalize()}')
+                    print()
+                    break
+
+        option = input("Hit return to generate a new password or enter 'q' to end.: ")
 
 
 def main():
-    file = 'words.txt'
+    # Replace word2.txt with the any text file name.
+    file = 'words2.txt'
+    # Generate password.
     password(file)
 
 if __name__ == '__main__':
