ce112beeec6616691d7ecdbcb08686cd1718854e
engrvitalis <ndlinks@gmail.com>
2021-01-24 23:33:44 +0100

    char_occurence initial doc-string

--- char_occurence.py
+++ char_occurence.py
@@ -1,2 +1,11 @@
 def char_occurence(file):
+    """
+    This program will take a file input and count the 
+    number of words that contains each character.
+
+    @param: String - file name
+    @return: Dictionary - with keys as character and value as 
+    the number of words they are contained in.
+    """
+
     
