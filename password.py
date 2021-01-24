2965d5c111acf498ac5f288158570939fbcd9546
engrvitalis <ndlinks@gmail.com>
2021-01-24 01:13:42 +0100

    Minor algorithm change.

--- password.py
+++ password.py
@@ -12,19 +12,22 @@ def password(file):
 
 
     # Declare variable.
-    ls = list()
+    word_list = list()
+    used_passwords = list()
 
-    # Give user loading commencement feedback.
-    print("Loading ...")
+    
 
  
     # Open text file and move words that met desired specification into ls.
     with open(file, 'r') as f:
+        # Give user loading commencement feedback.
+        print("Loading ...")
         print('...\n...')
+
         for line in f:
             for word in line.split():
                 if len(word) >= 3 and word.isalpha():
-                    ls.append(word)
+                    word_list.append(word)
     print('...')
     print("Program loaded successfully!\n")
     
@@ -39,25 +42,34 @@ def password(file):
         elif option == "":
             while True:
                 # Generate 2 random words from word list.
-                word1 = random.choice(ls)   
-                word2 = random.choice(ls)
+                word1 = random.choice(word_list)   
+                word2 = random.choice(word_list)
                 # if the numbers are the same or their lengths combined
                 # is not between 8 and 10, continue, else concatenate.
                 if word1 == word2 or not 8 <= len(word1 + word2) <= 10:
                     continue
                 else:
-                    print(f'Password: {word1.capitalize() + word2.capitalize()}')
-                    print()
-                    break
+                    pas = word1.capitalize() + word2.capitalize()
+                    # Making sure there is no password duplicates per session.
+                    if pas.lower() in used_passwords:
+                        continue
+                    else:
+                        used_passwords.append(pas.lower())
+                        print(f'Password: {pas}')
+                        print()
+                        break
 
-        option = input("Hit return to generate a new password or enter 'q' to end.: ")
+        option = input("Hit return to generate a new password or enter 'q' to end: ")
 
 
 def main():
-    # Replace word2.txt with the any text file name.
-    file = 'words2.txt'
-    # Generate password.
-    password(file)
+    # Request input from user.
+    file = input("\nEnter word file name: ")
+    try:
+        # Generate password.
+        password(file)
+    except:
+        print("Enter valid file name!")
 
 if __name__ == '__main__':
     main()
