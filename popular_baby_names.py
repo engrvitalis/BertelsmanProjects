5791a2f81f578a5fa42c994eccb7723cae0b1e47
engrvitalis <ndlinks@gmail.com>
2021-01-26 13:29:03 +0100

    separate names function removed

--- popular_baby_names.py
+++ popular_baby_names.py
@@ -12,26 +12,47 @@ def popular_names(files):
     @return: Tuple - containing 2 lists, most popular names for boys and girls
         respectively.
     """
-    pass
 
+    # Initialize variables.
+    popular_boys_names = list()
+    popular_girls_names = list()
 
-def separate_files(files):
-    # Separate male from female names.
-    boys_files = [name for name in files if 'Boys' in name]
-    girls_files = [name for name in files if 'Girls' in name]
-    # print(boys_files)
-    # print(girls_files)
-    return tuple([boys_files, girls_files])
-    
+    # Go through the boys files.
+    for file in files:
 
+        # Initialize variable.
+        name, num = None, None
 
+        # Read each file.
+        with open(file, 'r') as f_handle:
+            # Go through each line
+            if 'Boys' in file:
+                for line in f_handle:
+                    line = line.split()
+                    if num is None or num < int(line[1]):
+                        name, num = line[0], int(line[1])
+
+                if name not in popular_boys_names:
+                    popular_boys_names.append(name)
+                name, num = None, None
+            else:
+                for line in f_handle:
+                    line = line.split()
+                    if num is None or num < int(line[1]):
+                        name, num = line[0], int(line[1])
+
+                if name not in popular_girls_names:
+                    popular_girls_names.append(name)
+                name, num = None, None
+
+    print(popular_boys_names)
+    print(popular_girls_names)
 
 
 def main():
-    files = os.listdir('BabyNames')
-    # print(ls)
+    # files = os.listdir('BabyNames')
+    files = [os.path.join('BabyNames', file) for file in os.listdir('BabyNames')]
     popular_names(files)
-    print(separate_files(files))
 
 
 if __name__ == '__main__':
