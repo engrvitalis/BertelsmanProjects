97f13d348559fa66a0cd092ed64af63eac66b2be
engrvitalis <ndlinks@gmail.com>
2021-01-26 11:53:12 +0100

    baby boys files separated from the girls.

--- popular_baby_names.py
+++ popular_baby_names.py
@@ -5,19 +5,28 @@ def popular_names(ls):
     """
     This program will read all file in BabyNames directory, identify names 
     that are popular for at least one year and return two lists containing most 
-    popular names for boys and girls respectively.
+    popular names for boys and girls respectively and each of the list cannot contain
+    repeated values.
 
     @param: List - Names of files.
     @return: Tuple - containing 2 lists, most popular names for boys and girls
         respectively.
     """
 
-    pass
+    # Separate male from female names.
+    boys_files = [name for name in ls if 'Boys' in name]
+    girls_files = [name for name in ls if 'Girls' in name]
+    # print(boys_files)
+    # print(girls_files)
+
+    # for file in 
+
+
 
 
 def main():
     ls = os.listdir('BabyNames')
-    print(ls)
+    # print(ls)
     popular_names(ls)
 
 
