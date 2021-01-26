702d77ec47fcdebf2f93637073433cfbc912e603
engrvitalis <ndlinks@gmail.com>
2021-01-26 22:00:09 +0100

    sensitive file can be read in.

--- redact_text.py
+++ redact_text.py
@@ -1,9 +1,27 @@
-def redact_text(file, new_file, sensitive_words):
-    pass
+def redact_text(file, new_file, sensitive_file):
+
+
+    # Initialize variable.
+    sensitive_list = list()
+
+    # Process sensitive words into a list, ls
+    with open(sensitive_file) as f:
+        for line in f:
+            for word in line.split():
+                sensitive_list.append(word.strip())
+
+    print(sensitive_list)
+                
 
 
 def main():
-    pass
+    file = 'words2.txt'
+    new_file = 'words3.txt'
+    sensitive_words = 'sensitive_words.txt'
+
+    redact_text(file, new_file, sensitive_words)
+
+
 
 
 if __name__ == '__main__':
