ab4f91aad1da1ab2e8086a71b0ddb376eb6139c3
engrvitalis <ndlinks@gmail.com>
2021-01-26 11:07:16 +0100

    popular_names doc-string completed.

--- popular_baby_names.py
+++ popular_baby_names.py
@@ -1,2 +1,12 @@
 def popular_names(ls):
-    
+    """
+    This program will read all file in BabyNames directory, identify names 
+    that are popular for at least one year and return two lists containing most 
+    popular names for boys and girls respectively.
+
+    @param: List - Names of files.
+    @return: Tuple - containing 2 lists, most popular names for boys and girls
+        respectively.
+    """
+
+    pass
