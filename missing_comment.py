9c8227b09459a4dd14a33b2efdee5c5a6190aaf4
engrvitalis <ndlinks@gmail.com>
2021-01-28 00:40:51 +0100

    Program complete

--- missing_comment.py
+++ missing_comment.py
@@ -7,15 +7,21 @@ def missing_comment(file):
     @param: String - file
     @return: None
     """
-
+    
     with open(file) as f:
+        # Starting counter to keep track of line number
         counter = 1
 
         for line in f:
+            # Checking if line starts with #
             if line.lstrip().startswith('#'):
                 comment = True
+            # Checking if line a function definition but not preceeded by comment line
             elif comment is False and line.startswith('def '):
+                # Extract function name from line
                 func_name = line[line.index(' ')+1:line.index('(')]
+
+                # Print output
                 print(f'\nFunction name: {func_name}')
                 print(f'File name: {file}')
                 print(f'Line number: {counter}')
@@ -28,20 +34,30 @@ def missing_comment(file):
 
 
 def main():
+    # Initialize variables
     files = list()
     invalid_files = list()
 
     while True:
+        # Request input from user until blank line is entered.
         file = input("Enter file name or blank space to end: ")
-        if file == "":break
+        if file == "":
+            break
+
+        # Append all file names in files
         files.append(file)
     
+    # Check the content of all the files in files for functions
+    # that are not immediately preceeded by a comment. 
     for file in files:
         try:
             missing_comment(file)
         except:
+            # If a file name is invalid, add the name into invalid_files.
             invalid_files.append(file)
 
+    # If there were invalid files name entry by the user, 
+    # print them all out.
     if len(invalid_files) > 0:
         print("\nThese file(s) could not be accessed!:")
 
