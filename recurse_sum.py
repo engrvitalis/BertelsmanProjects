7affe07dfd12a7943ebbb710246c6729c64d987d
engrvitalis <ndlinks@gmail.com>
2021-01-28 20:41:28 +0100

    Commenting completed

--- recurse_sum.py
+++ recurse_sum.py
@@ -1,13 +1,26 @@
 def recurse_sum():
+    """
+    This function sums request number input from user and 
+    sums them recursively.
+
+    @param: None
+    @return: Float - Total sum of the numbers.
+    """
+
+    # Request input from the user.
     num = input("Enter a number or blank line to stop: ")
+
+    # Return total if the user enters blank line.
     if num == "":
         return 0.0
     else:
+        # Recursively sum numbers.
         return float(num) + recurse_sum()
 
 
 def main():
     try:
+        # Recursively sum numbers and display the total.
         print(f'\nTotal: {recurse_sum()}')
     except:
         print("\nInvalid Input! You must enter a float or an int: ")
