369a69a01a6adb62733f0ce7f0401c338f881ece
engrvitalis <ndlinks@gmail.com>
2021-01-28 19:29:44 +0100

    get_nums function doc-string and commenting complete.

--- recurse_sum.py
+++ recurse_sum.py
@@ -1,4 +1,5 @@
 def recurse_sum():
+    pass
 
 
 
@@ -6,13 +7,27 @@ def recurse_sum():
 
 
 def get_nums():
+    """
+    This function continuously request number input from
+    user until a blank line is entered. Then, it will return a list containing 
+    all the numbers entered by the user.
+
+    @param: None
+    @return: List
+    """
+
+    # Initialize variable.
     ls = list()
+
+    # Continuously request input from user until blank line is entered.
     while True:
         num = input("Enter a number or blank line to stop: ")
         if num == "":
             break
-
+        
+        # Add the number into ls.
         ls.append(float(num))
+        
     return ls
 
 
