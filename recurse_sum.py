5cb96ca49de57019b144982a7a6f09f5cf11af27
engrvitalis <ndlinks@gmail.com>
2021-01-28 20:30:35 +0100

    Program complete

--- recurse_sum.py
+++ recurse_sum.py
@@ -2,14 +2,15 @@ def recurse_sum():
     num = input("Enter a number or blank line to stop: ")
     if num == "":
         return 0.0
-
     else:
         return float(num) + recurse_sum()
 
 
 def main():
-    print(recurse_sum())
-
+    try:
+        print(f'\nTotal: {recurse_sum()}')
+    except:
+        print("\nInvalid Input! You must enter a float or an int: ")
 
 if __name__ == '__main__':
     main()
