a839a553a71b41bd7eb941ccfb9165784d04e25c
engrvitalis <ndlinks@gmail.com>
2021-01-28 20:13:47 +0100

    get_num function removed

--- recurse_sum.py
+++ recurse_sum.py
@@ -1,38 +1,14 @@
 def recurse_sum():
-    pass
+    num = input("Enter a number or blank line to stop: ")
+    if num == "":
+        return 0.0
 
-
-
-
-
-
-def get_nums():
-    """
-    This function continuously request number input from
-    user until a blank line is entered. Then, it will return a list containing 
-    all the numbers entered by the user.
-
-    @param: None
-    @return: List
-    """
-
-    # Initialize variable.
-    ls = list()
-
-    # Continuously request input from user until blank line is entered.
-    while True:
-        num = input("Enter a number or blank line to stop: ")
-        if num == "":
-            break
-        
-        # Add the number into ls.
-        ls.append(float(num))
-        
-    return ls
+    else:
+        return float(num) + recurse_sum()
 
 
 def main():
-    print(get_nums())
+    print(recurse_sum())
 
 
 if __name__ == '__main__':
