e71f1d6c871b5180cc298c7ad864b529df01e50f
engrvitalis <ndlinks@gmail.com>
2021-01-31 10:42:37 +0100

    edit_distance function doc-string complete.

--- edit_distance.py
+++ edit_distance.py
@@ -1,2 +1,8 @@
 def edit_distance(s, t):
-    
+    """
+    This function computes the minimum number of edits needed 
+    transform strings, s into t recursively.
+
+    @param: String - s, t
+    @return: Integer
+    """
