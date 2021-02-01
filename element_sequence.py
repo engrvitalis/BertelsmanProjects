f0afa5dd85d88389b6cbf11fe89a3b024a34e52d
engrvitalis <ndlinks@gmail.com>
2021-02-01 14:31:53 +0100

    main function defined

--- element_sequence.py
+++ element_sequence.py
@@ -1,2 +1,15 @@
-def element_sequence(name):
+def element_sequence(name, file):
+    with open(file) as f:
+        for line in f:
+            print(line.lower().split(',')[-1], end='')
+
+
+def main():
+    name = "Hydrogen"
+    file = "elements.txt"
     
+    element_sequence(name, file)
+
+
+if __name__ == '__main__':
+    main()
