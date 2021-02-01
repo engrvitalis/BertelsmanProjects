4e76ea5eeb89ba0b3bf6b918b2040c17051e7bc7
engrvitalis <ndlinks@gmail.com>
2021-02-01 23:58:27 +0100

    Debugging

--- element_sequence.py
+++ element_sequence.py
@@ -1,19 +1,26 @@
-def element_sequence(name, file):
-    with open(file) as f:
-        for line in f:
-            word = line.lower().split(',')
-            if word[-1][0] == name.lower()[-1]:
-                return name + word
+def element_sequence(name, ls):
+    if name not in ls:
+        return ""
+    elif 
+        return name + element_sequence(name, ls[:-1])
 
 
 def get_names(file):
+    names = list()
+
+    with open(file, 'r') as f:
+        for line in f:
+            
+            names.append(line.lower().split(',')[-1].strip())
     
+    return names
+
 
 def main():
     name = "Hydrogen"
     file = "elements.txt"
     
-    element_sequence(name, file)
+    element_sequence(name)
 
 
 if __name__ == '__main__':
