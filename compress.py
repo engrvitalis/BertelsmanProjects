f700a5101aa92866b5f1529698ae7bd603036a53
engrvitalis <ndlinks@gmail.com>
2021-02-03 22:27:55 +0100

    Comment completed

--- compress.py
+++ compress.py
@@ -1,36 +1,48 @@
 def compress(data):
     '''
-    This function takes a list or string argument, 
-    recursively compress it using run-length algorithm and 
-    return compressed list.
+    This function takes sequence of characters as argument, 
+    recursively compress them using run-length algorithm and 
+    return a compressed list.
 
     @param: List or String
     @return: List
     '''
 
+    # Initialize variable.
     ls = []
-    if len(data) == 0:
-        return ls
-    else:
-        for i in range(len(data)):
-            if data[i] != data[0]:
-                ls.extend([data[0], i])
-                return ls + compress(data[i:])
-        ls.extend([data[0], len(data)])
-        return ls
+
+    # Go through the characters.
+    for i in range(len(data)):
+        # Check if character is different from previous ones.
+        if data[i] != data[0]:
+            # Add character with its count into ls
+            ls.extend([data[0], i])
+            # Slice counted character from data and recursively process
+            # the new data, add it to ls and return it.
+            return ls + compress(data[i:])
+
+    # Return the last similar character(s) in data.
+    ls.extend([data[0], len(data)])
+
+    return ls
 
 
 def main():
+    # Initialize variable.
     data = list()
+    # Request input from user.
     while True:
         inp = input("Please, enter an input or blank line to stop: ")
         if inp == "":
             break
-
+        
+        # Add all user input into data.
         data.append(inp)
     
-    print(f'\nOriginal Data:\n{data}\n')
-    print(f'\nCompressed Data:\n{compress(data)}')
+    # Guard against blank line entry and display output.
+    if len(data) != 0:
+        print(f'\nOriginal Data:\n{data}\n')
+        print(f'\nCompressed Data:\n{compress(data)}')
 
 
 if __name__ == '__main__':
