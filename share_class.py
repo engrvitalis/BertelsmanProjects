2363cb3217b73048bf597eb338a4af872c2cbc7b
engrvitalis <ndlinks@gmail.com>
2021-02-05 11:46:48 +0100

    Initial commit

--- share_class.py
+++ share_class.py
@@ -11,19 +11,27 @@ def share_class(classes, instructors):
     with open(instructors) as f:
         for line in f:
             instructor_ls.append(line.strip())
+    instructor_ls = set(instructor_ls)
 
     items = class_dic.items()
     items_value_sorted = sorted([(item[1], item[0]) for item in items])
     items_sorted = [(item[1], item[0]) for item in items_value_sorted]
-    # items_sorted = items_sorted[::-1]
+    items_sorted = items_sorted[::-1]
 
     for instructor in instructor_ls:
         schedule[instructor] = []
 
+    i = 0
+    ind = 0
+
+    while i < len(items_sorted):
+        for name in schedule:
+            schedule[name].append(items_sorted[i])
+            i += 1
+            ind += 1
+
 
     print(class_dic)
-    # print(instructor_ls)
-    # print(items_value_sorted)
     print(items_sorted)
     print(schedule)
 
