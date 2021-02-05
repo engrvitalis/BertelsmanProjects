7972a5a8a82e977cfd149a3f0e18dfaedbbdbcb7
engrvitalis <ndlinks@gmail.com>
2021-02-05 11:48:00 +0100

    my_sum doc-string completed

--- summing_numbers.py
+++ summing_numbers.py
@@ -1,2 +1,10 @@
 def my_sum(*args):
+    """
+    This function takes a variable number of arguments from 
+    the user and return the sum.
+
+    @param: List, float, int, tuple.
+    @return: Int, float.
+    """
+
     
