4ee0110f92d33260bd1f15cde53d908426967904
engrvitalis <ndlinks@gmail.com>
2021-02-05 12:13:26 +0100

    More changes

--- test.py
+++ test.py
@@ -1,40 +1,69 @@
-# Find and display the names of Python functions that are not immediately preceded by a
-# comment.
-#
-from sys import argv
-# Verify that at least one file name has been provided as a command line argument
-if len(argv) == 1:
-    print("At least one filename must be provided as a", \
-        "command line argument.")
-    print("Quitting...")
-    quit()
-# Process each file provided as a command line argument
-for fname in argv[1 : len(argv)]:
-    # Attempt to process the file
-    try:
-        inf = open(fname, "r")
-        # As we move through the file we need to keep a copy of the previous line so that we
-        # can check to see if it starts with a comment character. We also need to keep track
-        # of the line number within the file.
-        prev = " "
-        lnum = 1
-        # Check each line in the current file
-        for line in inf:
-            # If the line is a function definition and the previous line is not a comment
-            if line.startswith("def ") and prev[0] != "#":
-                # Find the first ( on the line so that we know where the function name ends
-                bracket_pos = line.index("(")
-                name = line[4 : bracket_pos]
-                # Display information about the function that is missing its comment
-                print("%s line %d: %s" % (fname, lnum, name))
-            # Save the current line and update the line counter
-            prev = line
-            lnum = lnum + 1
-        # Close the current file
-        inf.close()
-    except:
-        print("A problem was encountered with file '%s'." % fname)
-        print("Moving on to the next file...")
+from random import randint
+
+def guess_num():
+    ''' guess a random number between 0 and 100. while users guess is equal to the number provide clues'''
+    num = randint(0,100)
+    while True:
+        try:
+            # get the number and check
+            guess = int(input('enter your guess: '))
+            if num > guess:
+                print('too low')
+            elif num < guess:
+                print('too high')
+            else:
+                print('That is correct. You won')
+                break
+        except Exception as e:
+            print('Did you enter a valid number? The number must be between 0 and 100')
+    
+
+
+def main():
+    print("Guessing Game\nCan you guess my number?\nIt's between 0 and 100")
+    guess_num()
+
+if __name__ == '__main__':
+    main()
+
+
+# # Find and display the names of Python functions that are not immediately preceded by a
+# # comment.
+# #
+# from sys import argv
+# # Verify that at least one file name has been provided as a command line argument
+# if len(argv) == 1:
+#     print("At least one filename must be provided as a", \
+#         "command line argument.")
+#     print("Quitting...")
+#     quit()
+# # Process each file provided as a command line argument
+# for fname in argv[1 : len(argv)]:
+#     # Attempt to process the file
+#     try:
+#         inf = open(fname, "r")
+#         # As we move through the file we need to keep a copy of the previous line so that we
+#         # can check to see if it starts with a comment character. We also need to keep track
+#         # of the line number within the file.
+#         prev = " "
+#         lnum = 1
+#         # Check each line in the current file
+#         for line in inf:
+#             # If the line is a function definition and the previous line is not a comment
+#             if line.startswith("def ") and prev[0] != "#":
+#                 # Find the first ( on the line so that we know where the function name ends
+#                 bracket_pos = line.index("(")
+#                 name = line[4 : bracket_pos]
+#                 # Display information about the function that is missing its comment
+#                 print("%s line %d: %s" % (fname, lnum, name))
+#             # Save the current line and update the line counter
+#             prev = line
+#             lnum = lnum + 1
+#         # Close the current file
+#         inf.close()
+#     except:
+#         print("A problem was encountered with file '%s'." % fname)
+#         print("Moving on to the next file...")
 
 # import re
 
