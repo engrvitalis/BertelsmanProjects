1ad647c1330c4a2d9774bf2e1c56edb54fd766f5
engrvitalis <ndlinks@gmail.com>
2021-02-06 12:14:05 +0100

    run_timer doc-string completed

--- run_timing.py
+++ run_timing.py
@@ -1,2 +1,10 @@
 def run_timer():
+    """
+    This function will constantly prompt the user to provide 
+    the time it took for the user to complete each 10 km in a race 
+    until the end of the race. It will then return the average
+    time it took to complete the whole race.
     
+    @param: int or float
+    @return: float
+    """
