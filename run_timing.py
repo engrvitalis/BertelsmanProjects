c6d7f8dd8d6da09df302127786b2e3f2958a43ee
engrvitalis <ndlinks@gmail.com>
2021-02-06 12:14:46 +0100

    run_timer function complete

--- run_timing.py
+++ run_timing.py
@@ -8,3 +8,25 @@ def run_timer():
     @param: int or float
     @return: float
     """
+
+    # Initialize variables
+    total = 0
+    count = 0
+
+    while True:
+        one_run = input("Enter 10 km run time: ")
+        # Stop if blank line is entered.
+        if not one_run:
+            break
+        else:
+            # Catch non numeric entry by user.
+            try:
+                total += float(one_run) # Update total.
+            except ValueError:
+                print("Invalid Entry. It must be int or float or blank line to terminate!")
+                continue
+            count += 1  # Update entry count.
+    
+    return total, count
+    
+
