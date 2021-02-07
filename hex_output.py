ebf4c81c05da1e8060a2bbf1d9d066c0bd6517f9
engrvitalis <ndlinks@gmail.com>
2021-02-07 08:53:35 +0100

    hex_output function doc-string complete.

--- hex_output.py
+++ hex_output.py
@@ -1,5 +1,15 @@
 def hex_output(hex_num):
-    pass
+    """
+    This function will take a hexadecimal number input
+    from the user and convert it to decimal.
+
+    @param: int
+    @return: int
+
+    eg:
+        hex_output(50) --> 80
+        hex_output(50) --> 32
+    """
 
 
 def main():
