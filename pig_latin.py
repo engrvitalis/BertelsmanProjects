1b3d2ceb225b7c99f902294b767fa46f29903815
engrvitalis <ndlinks@gmail.com>
2021-02-07 21:06:53 +0100

    main function completed

--- pig_latin.py
+++ pig_latin.py
@@ -1,4 +1,4 @@
-def pig_latin():
+def pig_latin(word):
     """
     This function takes an English word as input and returns a pig latin version of 
     of the it.
@@ -6,3 +6,17 @@ def pig_latin():
     @param: str
     @return: str
     """
+
+    if word[0] in 'aeiou':
+        return word + 'way'
+
+    return word[1:] + word[0] + 'ay'
+
+
+def main():
+    print("\nThis program translates English words to pig latin.")
+    word = input("Enter an English word: ")
+    try:
+        print(f"The pig latin version of '{word}' is {pig_latin(word)}")
+    except ValueError:
+        print("Invalid entry!")
