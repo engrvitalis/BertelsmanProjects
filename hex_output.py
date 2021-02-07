65f102433f72054428766cc2902b987e8af2d8e7
engrvitalis <ndlinks@gmail.com>
2021-02-07 10:28:19 +0100

    hex_output function complete

--- hex_output.py
+++ hex_output.py
@@ -3,17 +3,25 @@ def hex_output(hex_num):
     This function will take a hexadecimal number input
     from the user and convert it to decimal.
 
-    @param: int
+    @param: str
     @return: int
 
     eg:
         hex_output(50) --> 80
-        hex_output(50) --> 32
+            Conversion procedure:
+                0 * 16**0 + 5 * 16**1 = 80
+
+        hex_output('80E1') --> 32993
+            Conversion procedure:
+                1 * 16**0 + 14 * 16**1 + 0 * 16**2 + 8 * 16**3 = 32993
     """
+    # Iterate over each character of the reversed hex_num string,
+    # convert to decimal and sum all.
+    return sum([int(str(j), base=16) * 16**i for i, j in enumerate(reversed(hex_num))])
 
 
 def main():
-    pass
+    print(hex_output('80e1'))
 
 
 if __name__ == '__main__':
