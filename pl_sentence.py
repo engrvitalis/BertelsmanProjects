b37662e3c50cf8b81e2b5825d00f1943f88c9d93
engrvitalis <ndlinks@gmail.com>
2021-02-08 23:21:26 +0100

    Program complete

--- pl_sentence.py
+++ pl_sentence.py
@@ -1,5 +1,14 @@
 def pl_sentence(sentence):
-    
+    """
+    This function will take a sentence as input and return 
+    pig latin sentence after translation.
+
+    @param: String
+    @return: String
+    """
+
+    return ' '.join([pig_latin(word) for word in sentence.split()])
+
 
 
 def pig_latin(word):
@@ -11,21 +20,35 @@ def pig_latin(word):
     @return: str
     """
 
+    # Flag capitalized words.
+    capitalized = word[0].isupper()
+
+    word = word.lower()
+
     # Translate words starting with vowel.
     if word[0] in 'aeiou':
-        return word + 'way'
+
+        # Translate words starting with consonants.
+        if capitalized:
+            return word.capitalize() + 'way'
+        else:
+            return word + 'way'
 
     # Translate words starting with consonants.
-    return word[1:] + word[0] + 'ay'
+    if capitalized:
+        return word[1:].capitalize() + word[0] + 'ay'
+    else:
+        return word[1:] + word[0] + 'ay'
 
 
 def main():
     # Request input from user.
-    print("\nThis program translates English words to pig latin.")
-    word = input("Enter an English word: ")
+    print("\nThis program translates English sentence to Pig Latin sentence.")
+    sentence = 'This is the town of Eggs' #input("Enter an English sentence: ")
     # Translate only if input is valid, else display error message.
     try:
-        print(f"The pig latin version of '{word}' is '{pig_latin(word.lower())}'")
+        print(f'Original sentence: {sentence}')
+        print(f'Pig latin sentence: {pl_sentence(sentence)}\n')
     except ValueError:
         print("Invalid entry!")
 
