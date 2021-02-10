93ec02b66da5830ec03f722125692edd78de45d2
engrvitalis <ndlinks@gmail.com>
2021-02-10 08:57:39 +0100

    Commenting of ubi_dubbi function complete.

--- ubi_dubbi.py
+++ ubi_dubbi.py
@@ -7,13 +7,15 @@ def ubi_dubbi(word):
     @return: str
     """
 
+    # Go through chars in word, if char is a vowel, precede char with 'ub' and add
+    # to list else just add char to the list. Finally, convert list to string.
     return ''.join(['ub'+char if char in 'aeiou' else char for char in word.lower()])
 
 
 def main():
     print("Translate a word to Ubi Dubbi!")
     word = input("Enter a word: ")
-    print(ubi_dubbi(word))
+    print(f'The Ubi Dubbi version of "{word}" is "{ubi_dubbi(word)}"')
 
 
 if __name__ == '__main__':
