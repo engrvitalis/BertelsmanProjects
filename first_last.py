fe563752ae63e4c13288a985a5e936d7179cfba0
engrvitalis <ndlinks@gmail.com>
2021-02-11 19:27:45 +0100

    function doc-string complete

--- first_last.py
+++ first_last.py
@@ -1,2 +1,13 @@
 def first_last(x):
-    pass
+    """
+    This function retrieves the first and last elements in 
+    x, join and return them of type x.
+
+    @param: x - any type
+    @return: same type as x
+    """
+
+    
+
+
+first_last('first')
