8a6629da61330b71f70ce1e84d059a3adfb784a5
engrvitalis <ndlinks@gmail.com>
2021-02-12 09:56:29 +0100

    Commenting complete

--- first_last.py
+++ first_last.py
@@ -3,9 +3,13 @@ def first_last(inp):
     This function retrieves the first and last elements in 
     inp, join and return them of type inp.
 
-    @param: inp - any type
+    @param: inp - str, list and tuple
     @return: same type as inp
     """
+
+    # Check inp type, concatenate its first and last elements
+    # and return as same type as inp.
+    # Display error message if type is not subscriptable.
     if type(inp) == str:
         return inp[0] + inp[-1]
     elif type(inp) == list:
@@ -15,7 +19,7 @@ def first_last(inp):
     else:
         return 'Object is not subscriptable!'
 
-
+# Call and test function.
 print(first_last([2,4, 5, 6]))
 print(first_last((2,4, 5, 6)))
 print(first_last('string'))
