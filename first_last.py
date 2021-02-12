87fc6e09334d78d929fe48af3cd2c9085457de4a
engrvitalis <ndlinks@gmail.com>
2021-02-12 09:49:25 +0100

    Program produces correct output

--- first_last.py
+++ first_last.py
@@ -1,13 +1,23 @@
-def first_last(x):
+def first_last(inp):
     """
     This function retrieves the first and last elements in 
-    x, join and return them of type x.
+    inp, join and return them of type inp.
 
-    @param: x - any type
-    @return: same type as x
+    @param: inp - any type
+    @return: same type as inp
     """
+    if type(inp) == str:
+        return inp[0] + inp[-1]
+    elif type(inp) == list:
+        return [inp[0], inp[-1]]
+    elif type(inp) == tuple:
+        return (inp[0], inp[-1])
+    else:
+        return 'Object is not subscriptable!'
 
-    
 
-
-first_last('first')
+print(first_last([2,4, 5, 6]))
+print(first_last((2,4, 5, 6)))
+print(first_last('string'))
+print(first_last({2,4,6}))
+print(first_last({'a':3, 'b':4}))
