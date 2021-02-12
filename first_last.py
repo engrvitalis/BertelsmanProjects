1c67e13442ca9bd4a70df546afa25f178c1d4008
engrvitalis <ndlinks@gmail.com>
2021-02-12 10:22:58 +0100

    Test cases adjustment

--- first_last.py
+++ first_last.py
@@ -20,11 +20,14 @@ def first_last(inp):
     else:
         return 'Object is not subscriptable!'
 
-# Call and test function.
+# Call and test the function.
 print()
-print(first_last([2,4, 5, 6]))
-print(first_last((2,4, 5, 6)))
-print(first_last('string'))
-print(first_last({2,4,6}))
-print(first_last({'a':3, 'b':4}))
+print(first_last('a'))              # Output: aa
+print(first_last(['b']))            # Output: ['b', 'b']
+print(first_last(['1', 3]))         # Output: ['1', 3]
+print(first_last([2,4, 5, 6]))      # Output: [2, 6]
+print(first_last((4,4, 5, 1)))      # Output: [4, 1]
+print(first_last('string'))         # Output: sg
+print(first_last({2,4,6}))          # Output: Object is not subscriptable!
+print(first_last({'a':3, 'b':4}))   # Output: Object is not subscriptable!
 print()
