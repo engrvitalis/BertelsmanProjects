b44a459cef70ba09a4d01f6cfd80105fd469bdd3
engrvitalis <ndlinks@gmail.com>
2021-02-13 22:16:58 +0100

    Additional comments and corrections.

--- alphabetizing_names.py
+++ alphabetizing_names.py
@@ -1,21 +1,29 @@
-def alphabetize_names(obj):
+def alphabetize_names(obj, key1, key2):
     """
-    This function takes list of dictionaries containing 
-    lastname, firstname and email and returns same sorted in 
-    ascending order by lastname and firstname.
+    This is a multi key sorting function.
 
-    @param: list - obj
+    It takes three arguments: obj, key1 and key2. 
+    obj is a list or tuple of dictionaries containing first, 
+    last and email keys. key1 and key2 are sorting keys which can 
+    be either first, last or email. When called with obj and the 
+    keys, it returns same obj sorted by key1 and then key2.
+
+    eg:
+        alphabetize_names(PEOPLE, 'first', 'second')
+        --> return PEOPLE sorted by first, and then second key.
+
+    @param: list - obj; any immutable object type for keys - key1 and key2
     @return: list - sorted list
     """
 
-    from operator import itemgetter
+    import operator as op
     
     
-    # Sort obj dicts with last and first name.
-    s = sorted(obj, key=itemgetter('last', 'first'))
+    # Sort obj dicts by last and first name keys.
+    s = sorted(obj, key=op.itemgetter(key1, key2))
 
     print()
-    # Print the sorted list contents.
+    # Print the sorted list content.
     for p in s:
         print(f'{p["last"]}, {p["first"]}: {p["email"]}')
     print()
@@ -25,5 +33,5 @@ PEOPLE = [{'first':'Reuven', 'last':'Lerner','email':'reuven@lerner.co.il'},
     {'first':'Vladimir', 'last':'Putin','email':'president@kremvax.ru'}
     ]
 
-# Sort PEOPLE in ascending order with first and last name.
-alphabetize_names(PEOPLE)
+# Sort PEOPLE in ascending order by last and first name.
+alphabetize_names(PEOPLE, 'last', 'first')
