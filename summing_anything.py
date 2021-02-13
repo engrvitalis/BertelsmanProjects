d6352085240dee2a9a9d23f409f25440c761d60f
engrvitalis <ndlinks@gmail.com>
2021-02-13 11:48:37 +0100

    function doc-string complete

--- summing_anything.py
+++ summing_anything.py
@@ -1,4 +1,14 @@
 def mySum(*items):
+    """
+    This function takes variable number of arguments.
+    It returns empty tuple if called with no arguments,
+    sum if called with int or float arguments or it will
+    concatenate arguments elements if called with str, list or
+    tuple.
+    
+    @param: *items: int, float, list or tuple.
+    @return: int, float, list or tuple.
+    """
     # Return empty tuple if no argument was provided.
     if not items:
         return items 
@@ -11,7 +21,9 @@ def mySum(*items):
     return output
 
 # Test calls.
-print(mySum([1,2,3], [4,5,6,[2,4]]))
-print(mySum(1,2,3))
+print(mySum([1, 2, 3], [4, 5, 6, [2, 4]]))
+print(mySum([2,4,5], [3,5]))
+print(mySum(([2, 4], 2, (4, 5, 6))))
+print(mySum(1, 2, 3))
 print(mySum('1', 's', 't'))
 print(mySum())
