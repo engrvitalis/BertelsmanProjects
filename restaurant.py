c8e0d09ba40532895d06d342467f44db3c982a79
engrvitalis <ndlinks@gmail.com>
2021-02-16 23:53:06 +0100

    Program complete

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,39 @@
 def restaurant():
-    pass
+
+
+    menu = {'egusi': 150, 
+    'akpu': 150, 
+    'onugbu': 200, 
+    'okro': 150, 
+    'garri': 150, 
+    'nsala': 300, 
+    'rice': 150, 
+    'stew': 150, 
+    'isiewu': 1000
+    }
+    total = 0
+
+    print()
+    while True:
+        order = input("Order: ").strip().lower()
+        if not order:
+            break
+        
+        if order in menu:
+            total += menu[order]
+            print(f'{order} cost {menu[order]}, total is {total}')
+        else:
+            print(f'Sorry, we are fresh out of {order} today.')
+
+    print(f'Your total is {total}')
+    return total
+
+
 
 
 def main():
     restaurant()
+    print()
+
+
+main()
