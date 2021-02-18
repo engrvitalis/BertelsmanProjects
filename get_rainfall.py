a898b2cf9ab0c550cef3a7ceadf884c9b489b7d4
engrvitalis <ndlinks@gmail.com>
2021-02-18 08:39:56 +0100

    get_rainfall doc-string completed.

--- get_rainfall.py
+++ get_rainfall.py
@@ -1,2 +1,12 @@
 def get_rainfall():
+    """
+    This program continuously prompts the user for name of city
+    and quantity of rainfall in mm. When the user enters blank space, 
+    the program is terminated and a dict is returned with city names 
+    and total rainfall for each city.
+
+    @param: None
+    @return: dict
+    """
+
     
