508bef94d43dec74155f92c6c29687bb28df3c3f
engrvitalis <ndlinks@gmail.com>
2021-02-19 09:25:35 +0100

    function complete

--- dictdiff.py
+++ dictdiff.py
@@ -8,4 +8,22 @@ def dictdiff(first, second):
     @return: dict
     """
 
-    
+    output = dict()
+
+    all_keys = first.keys() | second.keys()
+
+
+    for key in all_keys:
+        if first.get(key) != second.get(key):
+            output[key] = [first.get(key), second.get(key)]
+
+    return output
+
+d1 = {'a': 1, 'b': 2, 'c': 3}
+d2 = {'a': 1, 'b': 2, 'c': 4}
+d3 = {'a': 1, 'b': 2, 'd': 3}
+
+print(dictdiff(d1, d1))
+print(dictdiff(d1, d2))
+print(dictdiff(d1, d3))
+print(dictdiff(d2, d3))
