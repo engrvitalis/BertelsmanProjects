3947a236eafad073d2fef03c021bb07812adaff9
engrvitalis <ndlinks@gmail.com>
2021-02-19 09:12:03 +0100

    Doc-string complete

--- dictdiff.py
+++ dictdiff.py
@@ -1,2 +1,11 @@
 def dictdiff(first, second):
-    pass
+    """
+    This function takes two dicts as arguments and returns a new dict 
+    that expresses the difference between the two dicts. 
+    If there are no differences between the dicts, dictdiff returns an empty dict.
+
+    @param: dict - first, second
+    @return: dict
+    """
+
+    
